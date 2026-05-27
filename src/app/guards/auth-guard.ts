004871b756a3041f1bf84867cae4169fd74f47fc
Maycool Vinicius <maycoolvinicius_nh_@hotmail.com>
2026-05-27 17:44:29 -0300

    Adiciona AuthGuard

--- src/app/guards/auth-guard.ts
+++ src/app/guards/auth-guard.ts
@@ -1,9 +1,11 @@
-import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 export const authGuard: CanActivateFn = () => {
 
   const router = inject(Router);
+  const toast = inject(ToastrService);
 
   const token = localStorage.getItem('token');
 
@@ -11,6 +13,9 @@ export const authGuard: CanActivateFn = () => {
     return true;
   }
 
+  toast.warning('Faça login para acessar');
+
   router.navigate(['/login']);
+
   return false;
 };
